7a6ab2241e07c00396a83924961b2ce88382bbca
kristina-arezina <arezina.kristina23@gmail.com>
2020-10-25 08:44:34 -0400

    sa works

--- tweepy_streamer.py
+++ tweepy_streamer.py
@@ -3,10 +3,15 @@ from tweepy import Cursor
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+
+from textblob import TextBlob
  
 import twitter_credentials
+
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import re
 
 
 # # # # TWITTER CLIENT # # # #
@@ -94,8 +99,22 @@ class TweetAnalyzer():
     """
     Functionality for analyzing and categorizing content from tweets.
     """
+
+    def clean_tweet(self, tweet):
+        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+    def analyze_sentiment(self, tweet):
+        analysis = TextBlob(self.clean_tweet(tweet))
+        
+        if analysis.sentiment.polarity > 0:
+            return 1
+        elif analysis.sentiment.polarity == 0:
+            return 0
+        else:
+            return -1
+
     def tweets_to_data_frame(self, tweets):
-        df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['Tweets'])
+        df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['tweets'])
 
         df['id'] = np.array([tweet.id for tweet in tweets])
         df['len'] = np.array([len(tweet.text) for tweet in tweets])
@@ -114,12 +133,9 @@ if __name__ == '__main__':
 
     api = twitter_client.get_twitter_client_api()
 
-    tweets = api.user_timeline(screen_name="realDonaldTrump", count=20)
-
-# Things you can look for in the tweet
-    # print(dir(tweets[0]))
-    # print(tweets[0].retweet_count)
+    tweets = api.user_timeline(screen_name="realDonaldTrump", count=200)
 
     df = tweet_analyzer.tweets_to_data_frame(tweets)
-    
-    print(df.head(15))
+    df['sentiment'] = np.array([tweet_analyzer.analyze_sentiment(tweet) for tweet in df['tweets']])
+
+    print(df.head(10))
